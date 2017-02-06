e79542127859f4971d62601dbb3163f1373ff086
Taylor <tbuchhei@gmail.com>
2017-02-05 21:11:02 -0800

    My first working AngularJS App! Finished Assignment 1

--- module1-solution/app.js
+++ module1-solution/app.js
@@ -3,13 +3,42 @@
     angular.module('LunchCheck', [])
     .controller('LunchCheckerController', LunchCheckerController);
     
-    LunchCheckerController.$inject = [$scope];
+    LunchCheckerController.$inject = ['$scope'];
     function LunchCheckerController($scope){
-        $scope.lunchChecked = "test";
-    }
+        $scope.message = "";
+        $scope.messageColor = "";
+        $scope.border = "";
+        $scope.enteredText = "";
+        $scope.checkSize = function(){
+            
+            var lunchItems = $scope.enteredText.split(",");
 
-    function LunchCheck(){
-        
-    }
+            //this removes cases where a space count as an item, ie case like sandwhich, chips, , pepsi 
+            //only counts as three instead of four
+            for(var i = 0; i < lunchItems.length; i++){
+                lunchItems[i] = lunchItems[i].replace(/\s/g, '');
+                if(lunchItems[i] == ""){
+                    lunchItems.splice(i, 1);
+                }
+            }
+
+            console.log(lunchItems.length);
+            if(lunchItems[0] === "" || lunchItems.length === 0){
+                $scope.message = "Please enter data first.";
+                $scope.messageColor = "black";
+                $scope.border = "1px solid red";
+            }
+            else if(lunchItems.length > 3){
+                $scope.message = "Too much!";
+                $scope.messageColor = "red";
+                $scope.border = "1px solid green";
+            }
+            else{
+                $scope.message = "Enjoy!";
+                $scope.messageColor = "green";
+                $scope.border = "1px solid green";
+            }
+        };
+    };
 
 })();
